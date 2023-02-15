2859f9421d324cac5199b99020be9bba038d1ef8
Thomas Murzyn <murzynthomas@gmail.com>
2023-02-15 15:10:49 +0100

    Fixed a name bug

--- src/components/store-articles/store-articles.component.tsx
+++ src/components/store-articles/store-articles.component.tsx
@@ -4,23 +4,21 @@ import { useAppSelector } from "../../app/hooks/hooks";
 import { selectArticles } from "../../app/features/articles/articles.selector";
 import StoreArticle from "../store-article/store-article.component";
 
-function UserStoreArticles() {
+function StoreArticles() {
   const articles = useAppSelector(selectArticles);
 
   return (
-    <>
-      <StoreContainer>
-        {articles.map((article) => {
-          return (
-            <StoreArticle
-              key={Math.floor(Math.random() * 10000)}
-              article={article}
-            />
-          );
-        })}
-      </StoreContainer>
-    </>
+    <StoreContainer>
+      {articles.map((article) => {
+        return (
+          <StoreArticle
+            key={Math.floor(Math.random() * 10000)}
+            article={article}
+          />
+        );
+      })}
+    </StoreContainer>
   );
 }
 
-export default UserStoreArticles;
+export default StoreArticles;
--- src/routes/shop/shop.component.tsx
+++ src/routes/shop/shop.component.tsx
@@ -1,5 +1,5 @@
 import { ShopContainer } from "./shop.styles";
-import StoreArticles from "../../components/user-articles-store/user-articles-store.component";
+import StoreArticles from "../../components/store-articles/store-articles.component";
 
 function Shop() {
   return (
