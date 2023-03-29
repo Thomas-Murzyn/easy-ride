ef4a9ea637a525fa2ca41c524b8a11be5f4a00c0
Thomas Murzyn <murzynthomas@gmail.com>
2023-03-29 10:42:03 +0200

    Add route to article in shop

--- src/routes/shop/shop.component.tsx
+++ src/routes/shop/shop.component.tsx
@@ -1,11 +1,21 @@
 import { ShopContainer } from "./shop.styles";
 import StoreArticles from "../../components/store-articles/store-articles.component";
+import { Routes, Route } from "react-router-dom";
+import Article from "../../components/article/article.component";
 
 function Shop() {
   return (
-    <ShopContainer data-testid="shop-component">
-      <StoreArticles />
-    </ShopContainer>
+    <Routes>
+      <Route
+        index
+        element={
+          <ShopContainer data-testid="shop-component">
+            <StoreArticles />
+          </ShopContainer>
+        }
+      />
+      <Route path="/article/:id" element={<Article />} />
+    </Routes>
   );
 }
 
