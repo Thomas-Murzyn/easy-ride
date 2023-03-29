5c21b0b883ee897f7d1d14ed802a6081eba76b33
Thomas Murzyn <murzynthomas@gmail.com>
2023-03-29 10:43:53 +0200

    Add route to article in shop

--- src/routes/shop/shop.test.tsx
+++ src/routes/shop/shop.test.tsx
@@ -2,14 +2,21 @@ import Shop from "./shop.component";
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import store from "../../app/store";
+import { BrowserRouter } from "react-router-dom";
+
+const customRender = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Shop />
+      </BrowserRouter>
+    </Provider>
+  );
+};
 
 describe("Shop", () => {
   test("renders correctly", () => {
-    render(
-      <Provider store={store}>
-        <Shop />
-      </Provider>
-    );
+    render(customRender());
 
     expect(screen.getByTestId("shop-component")).toBeInTheDocument();
   });
