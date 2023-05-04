32d96d0e6708bdae6b480ed33f07bf43a52cf686
Thomas Murzyn <murzynthomas@gmail.com>
2023-05-04 09:38:08 +0200

    Add a statement that only allow an article owner to see the offer notification

--- src/components/store-article/store-article.component.tsx
+++ src/components/store-article/store-article.component.tsx
@@ -7,14 +7,32 @@ import {
 } from "./store-article.styles";
 import { Article } from "../../app/features/articles/articles.slice";
 import { useNavigate, useLocation } from "react-router-dom";
+import { useAppSelector } from "../../app/hooks/hooks";
+import { selectCurrentUser } from "../../app/features/user/user.selector";
 
 const StoreArticle = ({ article }: { article: Article }) => {
   const location = useLocation().pathname;
   const navigate = useNavigate();
+  const currentUser = useAppSelector(selectCurrentUser);
 
   const goToArticle = () => {
     navigate(`${location}/article/${article.id}`);
   };
+
+  if (article.offers.length > 0 && article.userId === currentUser?.userId) {
+    return (
+      <ArticleContainer onClick={goToArticle}>
+        <ArticlePictureContainer>
+          <img src={article.imageUrls[0]} alt={article.articleName} />
+        </ArticlePictureContainer>
+        <ArticleInfoContainer>
+          <ArticleInfo>{article.articleName}</ArticleInfo>
+          <ArticleInfo>Prix : {article.price}€</ArticleInfo>
+          <NotificationLogo>{article.offers.length}</NotificationLogo>
+        </ArticleInfoContainer>
+      </ArticleContainer>
+    );
+  }
 
   return (
     <ArticleContainer onClick={goToArticle}>
@@ -24,9 +42,6 @@ const StoreArticle = ({ article }: { article: Article }) => {
       <ArticleInfoContainer>
         <ArticleInfo>{article.articleName}</ArticleInfo>
         <ArticleInfo>Prix : {article.price}€</ArticleInfo>
-        {article.offers.length > 0 && (
-          <NotificationLogo>{article.offers.length}</NotificationLogo>
-        )}
       </ArticleInfoContainer>
     </ArticleContainer>
   );
